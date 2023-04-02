7c44b8b32fe12c91b949a3d7f0b8263c3aa2f5ca
Aabid Majid <aabidmajid@Aabids-MacBook-Air.local>
2023-04-02 07:13:50 +0530

    empty project

--- ui/src/Footer.js
+++
@@ -1,9 +0,0 @@
-function Footer() {
-    return (
-        <div id="footer">
-            <div>All rights reserved</div>
-            <a>Hello World</a>
-        </div>
-    )
-}
-export default Footer
